df9473d53ac2188d3a05e66df8a34fbdda533cb3
Gavin Brown <gavin.brown@uk.com>
2002-05-11 11:14:19 +0000

    fixed HTML error picked up by blimey.
    git-svn-id: http://php-mailto.googlecode.com/svn/trunk@8 5168cbd4-2c3e-11df-b9ae-e7b6a08c64db

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
-<!-- $Id: test.php,v 1.1.1.1 2002-02-11 13:27:26 jodrell Exp $ -->
+<!-- $Id: test.php,v 1.2 2002-05-11 11:14:19 jodrell Exp $ -->
 <?php include('mailto.php'); ?>
 <html>
 	<head>
-		<title>mailto.php test page</title.
+		<title>mailto.php test page</title>
 	</head>
 	<body>
 		<h1>mailto.php test page</h1>
