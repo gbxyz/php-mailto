01926e93ca5f1ebb87258ba531ae9590ee69fccb
Gavin Brown <gavin.brown@uk.com>
2004-07-06 13:50:34 +0000

    added a mailto_html() function which greps HTML strings and obfuscated e-mail addresses it finds. also added a test section.
    git-svn-id: http://php-mailto.googlecode.com/svn/trunk@15 5168cbd4-2c3e-11df-b9ae-e7b6a08c64db

--- test.php
+++ test.php
@@ -1,51 +1,104 @@
-<?include('mailto.php')?>
+<?require('mailto.php')?>
 <html>
 	<head>
 		<title>mailto.php test page</title>
 		<style type="text/css">
-			body {
-				margin:			10px 100px;
-				font-famiy:		Helvetica, Arial, Sans-Serif;
-				background-color:	#E2E2DE;
-				color:			#404040;
+			noscript {
+				display: inline;
 			}
 		</style>
+		<script language="JavaScript">
+			/*
+				this bit of code works around a bug in Mozilla. <noscript> tags should be
+				inline, but Mozilla renders them as blocks. So we explicity set the display
+				attribute in the CSS above, and then use JavaScript to correct it. When JavaScript
+				is turned off, the <noscript> tags render correctly, and when it's turned on they're
+				invisible.
+			*/
+			document.write('<style type="text/css">noscript { display: none; }</style>');
+		</script>
 	</head>
 	<body>
+
 		<h1>mailto.php test page</h1>
+
 		<p>This is the test page for mailto.php.</p>
 		<hr />
+
 		<h2>1. mailto() test:</h2>
 		<p>You can use the <tt>mailto()</tt> function to directly embed an obfuscated e-mail address link into some HTML, using the following syntax:</p>
+
 		<blockquote><pre>&lt;?mailto('user@host.com', 'click here to e-mail me')?&gt;</pre></blockquote>
+
 		<p>The second argument is optional.</p>
 		<p>Here's the test:</p>
+
 		<p>The administrator of this site is <?mailto($_SERVER[SERVER_ADMIN])?>.</p>
+
 		<hr />
+
 		<h2>2. smailto() test</h2>
+
 		<p>the <tt>smailto()</tt> function returns a string containting an obfuscated HTML link to an e-mail address. This function is useful if you want to store or buffer the link for use later on. The syntax is:</p>
+
 		<blockquote><pre>&lt;?php
 	$link = smailto('user@host.com', 'click here to e-mail me');
 	print $link;
 ?&gt;</pre></blockquote>
+
 		<p>The second argument is optional.</p>
 		<p>Here's the test:</p>
+
 		<?php
 			$string = smailto($_SERVER[SERVER_ADMIN]);
 			print '<p>The administrator of this site is '.$string.'</p>';
 		?>
 
 		<hr />
+
 		<h2>3. mailto_text() test</h2>
+
 		<p>When supplied a string, <tt>mailto_text()</tt> will attempt to replace any e-mail addresses it finds with obfuscated links.</p>
+
 		<blockquote><pre>&lt;php
-	$string = "&lt;p&gt;Here is some multiline text with an e-mail address (user@host.com) in it.&lt;/p&gt;";
+	$string = "&lt;p&gt;Here is some text with an e-mail address (user@host.com) in it.&lt;/p&gt;";
 	print mailto_text($string);
 ?&gt;</pre></blockquote>
+
 		<p>This produces:</p>
+
 		<?php
 			$string = "<p>Here is some multiline text with an e-mail address ($_SERVER[SERVER_ADMIN]) in it.</p>";
 			print mailto_text($string);
+		?>
+
+		<hr />
+
+		<h2>3. mailto_html() test</h2>
+
+		<p>When supplied a string, <tt>mailto_html()</tt> will attempt to replace any mailto: links it finds with obfuscated links.</p>
+
+		<blockquote><pre>&lt;php
+	$string = "&lt;p&gt;Here is some text with an &lt;a href="mailto:user@host.com"&gt;e-mail address&lt;/a&gt; in it.&lt;/p&gt;";
+	print mailto_text($string);
+?&gt;</pre></blockquote>
+
+		<p>This produces:</p>
+
+		<?php
+			$string = "<p>Here is some text with an <a 
+href=\"mailto:$_SERVER[SERVER_ADMIN]\"
+> 
+
+
+
+
+e-mail
+
+ address
+
+</a> in it.</p></p>";
+			print mailto_html($string);
 		?>
 
 		<hr />
